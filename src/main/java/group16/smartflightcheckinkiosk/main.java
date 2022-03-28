e19277fdf50d5693ca586dfdca0ddec75a01abac
xiaolinxiaolin <92717762+xialinxiaolin@users.noreply.github.com>
2022-03-28 18:00:29 +0800

    Add files via upload

---
+++ src/main/java/group16/smartflightcheckinkiosk/main.java
@@ -0,0 +1,59 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        try {
+            String filePath = "/home/danny/data.csv";
+            int infor;
+            infor[] Allinfor;
+            Allinfor = new infor[100];
+            for(int i=0;i<100;i++){
+                Allinfor[i]=new infor("","","","","","","");
+            }
+            readFile.readCsvFile(filePath,Allinfor);
+            System.out.println("请输入您的用户名:");
+            Scanner scanner1=new Scanner(System.in);
+            String sur=scanner1.nextLine();
+            System.out.println("请输入您的密码:");
+            Scanner scanner2=new Scanner(System.in);
+            String Id=scanner2.nextLine();
+            int sig=Allinfor[0].login(sur,Id,Allinfor);
+            if(sig==0){
+                System.out.println("登录失败!查无此人");
+            }else if(sig>0){
+                System.out.println("登录成功!");
+
+                while(true){
+                    switch (Allinfor[0].menu()){
+                        case 1:{
+                            Allinfor[0].changeChair(sig,Allinfor);
+                            break;
+                        }
+                        case 2:{
+                            Allinfor[0].changeFood(sig,Allinfor);
+                            break;
+                        }
+                        case 3:{
+                            Allinfor[0].allPrice(sig,Allinfor);
+                            break;
+                        }
+                        case 4:{
+                            readFile.readCsvFile(filePath,Allinfor);
+                            break;
+                        }
+                        case 5:{
+                            System.exit(0);
+                            break;
+                        }
+                    }
+                }
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/group16/smartflightcheckinkiosk/readFile.java
@@ -0,0 +1,30 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+import com.csvreader.*;
+public class readFile {
+    public static void readCsvFile(String filePath,infor []Allinfor) {
+        try {
+            ArrayList<String[]> csvList = new ArrayList<String[]>();
+            CsvReader reader = new CsvReader(filePath,',',Charset.forName("GBK"));
+//          reader.readHeaders(); //跳过表头,不跳可以注释掉
+
+            while(reader.readRecord()){
+                csvList.add(reader.getValues()); //按行读取，并把每一行的数据添加到list集合
+            }
+
+            for(int row=0;row<csvList.size();row++){
+                infor inf =new infor(csvList.get(row)[0],csvList.get(row)[1],csvList.get(row)[2],csvList.get(row)[3],csvList.get(row)[4],csvList.get(row)[5],csvList.get(row)[6]);
+                inf.add_infor(inf,Allinfor);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
