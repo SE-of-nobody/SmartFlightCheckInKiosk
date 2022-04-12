1839c93e07dc7e36e908df20118acf4c78b235c4
CarrisaZ <424319238@qq.com>
2022-04-12 17:19:03 +0800

    点boardingpass的ok弹出三个窗口并关闭当前窗口

--- src/main/java/group16/smartflightcheckinkiosk/BoardingpassController.java
+++ src/main/java/group16/smartflightcheckinkiosk/BoardingpassController.java
@@ -1,17 +1,46 @@
 package group16.smartflightcheckinkiosk;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.text.Text;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
+import java.io.IOException;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 public class BoardingpassController {
     @FXML
     private Label Passenger;
     @FXML
     private Label Staff;
     @FXML
-    protected void onOKClick() {
+    private Label label_ok;
+    @FXML
+    private Label label_help;
+    @FXML
+    public void onOKClick() throws IOException {
+        Stage primaryStage=(Stage)label_ok.getScene().getWindow();primaryStage.close();
+        ticketShow();boardingpassShow();tagShow();
         System.out.println("you choose ok");}
     @FXML
     protected void onHelpClick() {
         System.out.println("you choose help");}
+    public void ticketShow() throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("boardingpass-ticket.fxml"));
+        Stage primaryStage=new Stage();
+        primaryStage.setScene(new Scene(root,200,400));
+        primaryStage.setTitle("Check in book number"); primaryStage.show();
+    }
+    public void boardingpassShow() throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("boardingpass-boardingpass.fxml"));
+        Stage primaryStage=new Stage();
+        primaryStage.setScene(new Scene(root,200,200));
+        primaryStage.setTitle("Check in book number"); primaryStage.show();
+    }
+    public void tagShow() throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("boardingpass-tag.fxml"));
+        Stage primaryStage=new Stage();
+        primaryStage.setScene(new Scene(root,200,200));
+        primaryStage.setTitle("Check in book number"); primaryStage.show();
+    }
 }
