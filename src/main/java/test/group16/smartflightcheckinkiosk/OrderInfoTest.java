cc14d2af951753ce9730f480805b630c2b04e52c
FnMays <100862206+FnMays@users.noreply.github.com>
2022-05-14 16:12:37 +0800

    test

---
+++ src/main/java/test/group16/smartflightcheckinkiosk/OrderInfoTest.java
@@ -0,0 +1,185 @@
+package test.group16.smartflightcheckinkiosk.Passager.service; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* OrderInfo Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>5ÔÂ 14, 2022</pre> 
+* @version 1.0 
+*/ 
+public class OrderInfoTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: startService() 
+* 
+*/ 
+@Test
+public void testStartService() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: menu() 
+* 
+*/ 
+@Test
+public void testMenu() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setSeat(Scanner scanner) 
+* 
+*/ 
+@Test
+public void testSetSeat() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setMeal(Scanner scanner) 
+* 
+*/ 
+@Test
+public void testSetMeal() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: payment(Scanner scanner) 
+* 
+*/ 
+@Test
+public void testPayment() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: writeDisk(Scanner scanner) 
+* 
+*/ 
+@Test
+public void testWriteDisk() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: checkWithFile() 
+* 
+*/ 
+@Test
+public void testCheckWithFile() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: checkUsernameAndID(String username, String id) 
+* 
+*/ 
+@Test
+public void testCheckUsernameAndID() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: checkBookingNumber(String bookingNum) 
+* 
+*/ 
+@Test
+public void testCheckBookingNumber() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: showInfo() 
+* 
+*/ 
+@Test
+public void testShowInfo() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+/** 
+* 
+* Method: getCSV(String PATH, String  CHARSET) 
+* 
+*/ 
+@Test
+public void testGetCSV() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = OrderInfo.getClass().getMethod("getCSV", String.class, String.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: chooseWays(String choose, Scanner scanner) 
+* 
+*/ 
+@Test
+public void testChooseWays() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = OrderInfo.getClass().getMethod("chooseWays", String.class, Scanner.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: loginSuccessMsg(String username) 
+* 
+*/ 
+@Test
+public void testLoginSuccessMsg() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = OrderInfo.getClass().getMethod("loginSuccessMsg", String.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+} 
---
+++ src/main/java/test/group16/smartflightcheckinkiosk/OrderTest.java
@@ -0,0 +1,265 @@
+package test.group16.smartflightcheckinkiosk.Passager.service; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* Order Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>5ÔÂ 14, 2022</pre> 
+* @version 1.0 
+*/ 
+public class OrderTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: toString() 
+* 
+*/ 
+@Test
+public void testToString() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getCsvString() 
+* 
+*/ 
+@Test
+public void testGetCsvString() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getBookingNumber() 
+* 
+*/ 
+@Test
+public void testGetBookingNumber() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setBookingNumber(String bookingNumber) 
+* 
+*/ 
+@Test
+public void testSetBookingNumber() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getIDNumber() 
+* 
+*/ 
+@Test
+public void testGetIDNumber() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setIDNumber(String IDNumber) 
+* 
+*/ 
+@Test
+public void testSetIDNumber() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getSurname() 
+* 
+*/ 
+@Test
+public void testGetSurname() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setSurname(String surname) 
+* 
+*/ 
+@Test
+public void testSetSurname() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getMeal() 
+* 
+*/ 
+@Test
+public void testGetMeal() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setMeal(String meal) 
+* 
+*/ 
+@Test
+public void testSetMeal() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getCreditNumber() 
+* 
+*/ 
+@Test
+public void testGetCreditNumber() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setCreditNumber(String creditNumber) 
+* 
+*/ 
+@Test
+public void testSetCreditNumber() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getBalance() 
+* 
+*/ 
+@Test
+public void testGetBalance() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setBalance(double balance) 
+* 
+*/ 
+@Test
+public void testSetBalance() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getSeat() 
+* 
+*/ 
+@Test
+public void testGetSeat() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setSeat(String seat) 
+* 
+*/ 
+@Test
+public void testSetSeat() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getBagFee() 
+* 
+*/ 
+@Test
+public void testGetBagFee() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setBagFee(double bagFee) 
+* 
+*/ 
+@Test
+public void testSetBagFee() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getMealFee() 
+* 
+*/ 
+@Test
+public void testGetMealFee() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setMealFee(double mealFee) 
+* 
+*/ 
+@Test
+public void testSetMealFee() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getSeatFee() 
+* 
+*/ 
+@Test
+public void testGetSeatFee() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setSeatFee(double seatFee) 
+* 
+*/ 
+@Test
+public void testSetSeatFee() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getPayed() 
+* 
+*/ 
+@Test
+public void testGetPayed() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setPayed(byte payed) 
+* 
+*/ 
+@Test
+public void testSetPayed() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
