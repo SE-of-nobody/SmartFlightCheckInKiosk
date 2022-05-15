4b6264482679da07e71ef397d7077aa543e3f5e3
xialinxiaolin <jp2019213116@qmul.ac.uk>
2022-05-15 18:01:09 +0800

    Test

---
+++ src/main/java/test/group16/smartflightcheckinkiosk/LuggageTest.java
@@ -0,0 +1,35 @@
+package test.group16.smartflightcheckinkiosk;
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* Luggage Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>5�� 15, 2022</pre> 
+* @version 1.0 
+*/ 
+public class LuggageTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: FindLuggage(String IDNumber) 
+* 
+*/ 
+@Test
+public void testFindLuggage() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
