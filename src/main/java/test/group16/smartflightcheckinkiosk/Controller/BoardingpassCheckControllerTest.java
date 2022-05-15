8281e8257f2b0287b6c11792a154e4ea5ee76e40
xialinxiaolin <jp2019213116@qmul.ac.uk>
2022-05-15 19:13:49 +0800

    Test

---
+++ src/main/java/test/group16/smartflightcheckinkiosk/Controller/BoardingpassCheckControllerTest.java
@@ -0,0 +1,45 @@
+package test.group16.smartflightcheckinkiosk.Controller; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* BoardingpassCheckController Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>5ÔÂ 15, 2022</pre> 
+* @version 1.0 
+*/ 
+public class BoardingpassCheckControllerTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: onNextClick() 
+* 
+*/ 
+@Test
+public void testOnNextClick() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: onPrevClick() 
+* 
+*/ 
+@Test
+public void testOnPrevClick() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
